a5e4bfcf38afdde2f7dcd29d5eac1a44ad8da224
ssebastianoo <sebastianogirotto05@gmail.com>
2023-10-06 10:29:25 +0200

    cards

---
+++ src/lib/PlaylistCard.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	export let cover: string;
+	export let title: string;
+</script>
+
+<div class="card">
+	<img width="74" src={cover} alt="album cover" />
+	<h3>{title}</h3>
+</div>
+
+<style lang="scss">
+	@import 'src/variables';
+
+	.card {
+		display: flex;
+		align-items: center;
+		gap: 15px;
+		border-radius: 10px;
+		background-color: hsla(0, 0%, 100%, 0.1);
+		backdrop-filter: blur(10px);
+		overflow: hidden;
+		font-size: 0.8em;
+		width: calc(33% - 5px);
+	}
+
+	@media (max-width: 1200px) {
+		.card {
+			width: calc(50% - 5px);
+		}
+	}
+
+	@media (max-width: 650px) {
+		.card {
+			width: calc(100% - 5px);
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import Player from '$lib/Player.svelte';
+	import PlaylistCard from '$lib/PlaylistCard.svelte';
+	import Album from '$lib/assets/trench.jpg';
 
 	import Fa from 'svelte-fa';
 	import { faHouse, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
@@ -17,7 +19,13 @@
 		</div>
 	</div>
 	<div class="section main">
-		<h2>Good Evening</h2>
+		<h2>Hey there, I'm Seb!</h2>
+		<div class="list">
+			<PlaylistCard title="Projects" cover={Album} />
+			<PlaylistCard title="Social" cover={Album} />
+			<PlaylistCard title="Skills" cover={Album} />
+			<PlaylistCard title="Hobbies" cover={Album} />
+		</div>
 	</div>
 	<div class="section library">
 		<p>Your Library</p>
@@ -57,14 +65,32 @@
 			display: flex;
 			align-items: center;
 			gap: 15px;
+			cursor: pointer;
+
+			&:not(.active) {
+				opacity: 0.8;
+				transition: opacity 0.2s ease-in-out;
+
+				&:hover {
+					opacity: 1;
+				}
+			}
 		}
 	}
 
 	.main {
 		grid-column: 2 / 3;
 		grid-row: 1 / 3;
-		background-color: $background;
+		background: linear-gradient(146deg, rgba(66, 32, 35, 1) -20%, rgba(18, 18, 18, 1) 100%);
 		padding: 20px;
+
+		.list {
+			margin-top: 20px;
+			display: flex;
+			justify-content: space-between;
+			flex-wrap: wrap;
+			gap: 12px 0;
+		}
 	}
 
 	.library {
